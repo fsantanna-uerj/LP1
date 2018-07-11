5f5755aecc72010b682b521a7531192e90b87f0d
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-07-11 16:09:22 -0300

    ...

---
+++ Code/08-arquivos/04-bin.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct Data {
+    char dia;
+    char mes;
+    char ano;
+};
+
+int main (void) {
+    int v1;
+    char v2[13];
+    struct Data v3;
+
+    FILE* f = fopen("arq-03.bin", "rb");
+
+    fread(&v1, sizeof(int), 1, f);
+    fread(v2, sizeof(char), 12, f);
+    v2[12] = '\0';
+    fread(&v3, sizeof(struct Data), 1, f);
+
+    fclose(f);
+
+    printf("%X %s (%d/%d/%d)\n", v1, v2, v3.dia, v3.mes, v3.ano);
+    return 0;
+}
