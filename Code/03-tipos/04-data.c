2e4c82288db1a389b0995d5e789e4244d38393f2
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-09-11 18:13:04 -0300

    ...

---
+++ Code/03-tipos/04-data.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (void) {
+#if 0
+    unsigned char uc = 0;
+    while (1) {
+        printf("%d\n", uc);
+        uc = uc + 1;
+    }
+#elif 0
+    char sc = 0;
+    while (1) {
+        printf("%d\n", sc);
+        sc = sc + 1;
+    }
+#else
+    char sc = 0;
+    while (1) {
+        printf("%d: %c\n", sc, sc);
+        sc = sc + 1;
+    }
+#endif
+    return 0;
+}
